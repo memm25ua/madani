45b5feb490c8d40276553d317c7821d2d7f0fb15
memm25ua <memm25@alu.ua.es>
2023-08-02 22:52:25 +0200

    Add Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // You can add your JavaScript or TypeScript code here
+  </script>
+  
+  <div class="bg-gradient-to-r from-blue-400 to-purple-500 h-screen flex flex-col justify-center items-center text-white">
+    <h1 class="text-6xl font-extrabold mb-4 animate__animated animate__fadeIn">Madani's Portfolio</h1>
+    <p class="text-2xl mb-8 animate__animated animate__fadeIn animate__delay-1s">Software Developer & Tech Enthusiast</p>
+    <a href="/about" class="text-xl bg-white text-blue-500 px-6 py-2 rounded-full hover:bg-blue-200 transition-all duration-300 ease-in-out animate__animated animate__fadeIn animate__delay-2s">Learn More About Me</a>
+  </div>
+  
