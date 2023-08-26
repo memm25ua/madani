18c94836b4a594c68fd7413e52ac476fdb8fbec0
memm25ua <memm25@alu.ua.es>
2023-08-26 19:25:55 +0200

    feat: Add custom cursor and update top bar logo link

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  // Your JavaScript logic here
+  let skills = ['JavaScript', 'Svelte', 'Tailwind CSS'];
+</script>
+
+<style>
+  /* Add any custom styles if needed */
+</style>
+
+<main class=" min-h-screen p-4">
+  <section class="  p-6">
+    <h1 class="text-3xl font-bold mb-4">About Me</h1>
+    <p class="text-lg mb-4">
+      Placeholder for your bio.
+    </p>
+  </section>
+
+  <section class="  p-6 mt-6">
+    <h2 class="text-2xl font-bold mb-4">Skills</h2>
+    <ul>
+      {#each skills as skill}
+        <li class="text-lg mb-2">{skill}</li>
+      {/each}
+    </ul>
+  </section>
+
+  <section class="  p-6 mt-6">
+    <h2 class="text-2xl font-bold mb-4">Contact</h2>
+    <p class="text-lg">
+      Email: <a href="mailto:example@example.com" class="text-blue-500">example@example.com</a>
+    </p>
+  </section>
+</main>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    // Your JavaScript logic here
+    let projects = [
+      { name: 'Project 1', description: 'Description for Project 1', link: '#'},
+      { name: 'Project 2', description: 'Description for Project 2', link: '#'},
+      { name: 'Project 3', description: 'Description for Project 3', link: '#'}
+    ];
+  </script>
+  
+  <style>
+    /* Add any custom styles if needed */
+  </style>
+  
+  <main class=" min-h-screen p-4">
+    <section class="rounded-lg shadow-lg p-6">
+      <h1 class="text-3xl font-bold mb-4">My Projects</h1>
+    </section>
+  
+    <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+      {#each projects as project}
+        <div class="rounded-lg shadow-lg p-6">
+          <h2 class="text-2xl font-bold mb-4">{project.name}</h2>
+          <p class="text-lg mb-4">
+            {project.description}
+          </p>
+          <a href={project.link} class="text-blue-500">Learn More</a>
+        </div>
+      {/each}
+    </section>
+  </main>
+  
