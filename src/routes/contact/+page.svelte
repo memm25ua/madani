59797c399ac1a29b1eaf74070d768fdd438045c2
memm25ua <memm25@alu.ua.es>
2023-09-09 20:01:44 +0200

    refactor: improve TopBar dropdown animation

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,47 +3,59 @@
 </script>
 
 <main
-  class="min-h-full w-full flex flex-wrap font-primary flex-grow items-center justify-center"
+  class="min-h-full w-full flex flex-wrap font-primary flex-grow items-center justify-center bg-pampas dark:bg-codgray"
 >
-  <section class="container-col px-4">
+  <section class="container-col">
     <HeroImg
       imgSrc="assets/heroContact.png"
       title="Contact Me"
       textOnRight={false}
     />
   </section>
-  <section class="container-col px-4">
-    <form>
+  <section class="container-col">
+    <form
+      class="w-full max-w-lg bg-pampas-100 dark:bg-gray-800 p-4 rounded-xl shadow-md flex flex-col"
+    >
       <div class="mb-4">
-        <label for="name" class="block text-lg font-bold mb-2">Name:</label>
+        <label
+          for="name"
+          class="block text-2xl font-light text-codgray-700 dark:text-pampas"
+          >Name:</label
+        >
         <input
           type="text"
           id="name"
           name="name"
-          class="bg-transparent w-full p-2 border rounded"
+          class="bg-transparent w-full p-2 border rounded border-codgray dark:border-pampas"
           placeholder="Your Name"
         />
       </div>
 
       <div class="mb-4">
-        <label for="email" class="block text-lg font-bold mb-2">Email:</label>
+        <label
+          for="email"
+          class="block text-2xl font-light text-codgray-700 dark:text-pampas"
+          >Email:</label
+        >
         <input
           type="email"
           id="email"
           name="email"
-          class="w-full p-2 border rounded"
+          class="bg-transparent w-full p-2 border rounded border-codgray dark:border-pampas"
           placeholder="Your Email"
         />
       </div>
 
       <div class="mb-4">
-        <label for="message" class="block text-lg font-bold mb-2"
+        <label
+          for="message"
+          class="block text-2xl font-light text-codgray-700 dark:text-pampas"
           >Message:</label
         >
         <textarea
           id="message"
           name="message"
-          class="w-full p-2 border rounded"
+          class="bg-transparent w-full p-2 border rounded border-codgray dark:border-pampas"
           rows="4"
           placeholder="Your Message"
         ></textarea>
@@ -51,7 +63,7 @@
 
       <button
         type="submit"
-        class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+        class="relative border-perano-700 border-2 self-center w-20 hover:w-16 transition-all rounded-lg hover:bg-perano-700 text-perano-700 before:absolute before:bottom-2 before:h-0.5 hover:before:bottom-3 hover:text-pampas-100 hover:before:bg-pampas-100 before:transition-all  before:bg-perano-700 before:w-8 p-2 hover:bg-perano-dark"
       >
         Send
       </button>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,27 +1,40 @@
-<script>
+<script lang="ts">
   import HeroImg from "$lib/components/HeroImg.svelte";
+    import { circOut, cubicIn, cubicOut, elasticOut } from "svelte/easing";
+  import { blur, scale, slide } from "svelte/transition";
+
+  const animDuration = 400;
 
   let projects = [
     {
+      id: 1,
       name: "Crmble Trello Powerup",
       description:
         "Front-End Angular Developer at App Square S.L. Specialized in designing and developing user interfaces for a CRM platform.",
       link: "#",
       techStack: "Angular.js, HTML, CSS",
+      img: "assets/hero.png",
+      flipped: false,
     },
     {
+      id: 2,
       name: "iOS App with Firebase Backend",
       description:
         "Freelance iOS App developer. Initiated and executed a start-up focused iOS application development project.",
       link: "#",
       techStack: "Swift, SwiftUI",
+      img: "assets/hero.png",
+      flipped: false,
     },
     {
+      id: 3,
       name: "Real-time Game",
       description:
         "Personal project focused on real-time game development. Utilized Apache Kafka for synchronization.",
       link: "#",
       techStack: "Python, Apache Kafka",
+      img: "assets/hero.png",
+      flipped: false,
     },
   ];
 </script>
@@ -29,27 +42,53 @@
 <main
   class="min-h-full w-full flex flex-wrap font-primary flex-grow items-center justify-center"
 >
-  <section class="container-col p-6">
-    <HeroImg imgSrc="assets/heroProjects.png" title="Projects" />
+  <section class="container-col">
+    <HeroImg imgSrc="assets/heroProjects.png" title="PROJECTS" />
   </section>
 
-  <section class="container-col">
-    {#each projects as project}
-      <div class="rounded-lg shadow-lg p-6">
-        <h2 class="text-2xl font-bold mb-2">{project.name}</h2>
-        <p class="text-lg mb-2">
-          {project.description}
-        </p>
-        <p class="text-sm mb-4">
-          <strong>Tech Stack:</strong>
-          {project.techStack}
-        </p>
-        <a href={project.link} class="text-blue-500">Learn More</a>
-      </div>
-    {/each}
+  <section class="container-col w-1/2">
+    <div class="flex w-full h-full flex-wrap flex-row justify-center">
+      {#each projects as project}
+        <div
+          class="relative w-96 h-64 2xl:h-96 rounded-2xl {project.flipped
+            ? 'shadow-inner shadow-pampas-200'
+            : 'shadow-md shadow-pampas-300'}  lg:ml-5 bg-pampas-100 mb-6 dark:bg-codgray-900 p-6"
+        >
+          {#if project.flipped}
+            <button
+              on:click={() => {
+                project.flipped = false;
+              }}
+              class="h-full w-full"
+              in:blur={{ duration: animDuration , easing: circOut }}
+            >
+              <h2 class="text-2xl font-bold mb-2">{project.name}</h2>
+              <p class="text-lg mb-2">
+                {project.description}
+              </p>
+              <p class="text-sm mb-4">
+                <strong>Tech Stack:</strong>
+                {project.techStack}
+              </p>
+              <a href={project.link} class="text-blue-500">Learn More</a>
+            </button>
+          {:else}
+            <button
+              on:click={() => {
+                project.flipped = true;
+              }}
+              class="h-full w-full"
+              in:blur={{ duration: animDuration , easing: circOut }}
+            >
+              <img
+                src={project.img}
+                alt="PPP"
+                class="h-full object-contain rounded-2xl"
+              />
+            </button>
+          {/if}
+        </div>
+      {/each}
+    </div>
   </section>
 </main>
-
-<style>
-  /* Add any custom styles if needed */
-</style>
