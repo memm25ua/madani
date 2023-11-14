437f70ddb50b3f2dff907ba7f54e5971b6b1a688
memm25ua <memm25@alu.ua.es>
2023-11-14 15:22:13 +0100

    more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,23 +33,21 @@
   Please wait...
 {:else}
   <main
-    class=" select-none transition-colors overflow-hidden relative duration-500 bg-pampas-200 dark:bg-codgray-950 dark:text-pampas-100 text-codgray-800 min-h-screen w-screen font-primary lg:grid lg:grid-cols-1 lg:grid-rows-[auto,1fr]"
+    class=" select-none transition-colors overflow-hidden relative duration-500 bg-pampas-200 dark:bg-codgray-950 dark:text-pampas-100 text-codgray-800 h-screen w-screen font-primary"
   >
     <!-- Top Thin Bar -->
-    <div
+    <header
       class="lg:col-span-1 z-40 relative"
       style="view-transition-name: header;"
     >
       <TopBar {data} />
-    </div>
+    </header>
 
-    <div
-      class="lg:col-span-1 flex flex-col overflow-auto px-6 items-center justify-center"
-    >
-      <div class="max-w-screen-2xl w-full grow">
+    <main class="main-section">
+      <div class="w-full h-full max-w-screen-3xl grow">
         <slot />
       </div>
-    </div>
+    </main>
 
     <CircleCursor />
   </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class=" min-h-full w-full flex flex-wrap font-primary flex-grow items-center justify-center "
+  class="h-full w-full grow flex flex-wrap font-primary items-center justify-self-center self-center md:mt-40"
 >
   <div class="container-col">
     <HeroImg imgSrc="assets/hero.webp" />
